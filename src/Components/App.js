26f971802bd3ae46b5446c5fac9b3d4057a17ca5
Adrame <ndiakhate01998@gmail.com>
2021-09-15 11:13:56 +0200

    Happy to commit my first react js app

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Todo from './Todo';
+import '../Components/Todo.css';
+const App=()=>{
+    return (
+        <div className="app">
+            <Todo/>
+        </div>
+    )
+}
+export default App;
